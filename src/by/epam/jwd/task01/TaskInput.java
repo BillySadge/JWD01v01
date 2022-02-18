990b1746b0b27695abf051895b6d9c5a7c3d1d11
Billy <billywillneverreturn@gmail.com>
2022-02-18 22:22:54 +0300

    small fix

--- src/by/epam/jwd/task01/TaskInput.java
+++ src/by/epam/jwd/task01/TaskInput.java
@@ -1,6 +1,5 @@
 package by.epam.jwd.task01;
 
-import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TaskInput {
@@ -9,16 +8,12 @@ public class TaskInput {
         @SuppressWarnings("resources")
         Scanner scanner = new Scanner(System.in);
         int value;
-        while (true) {
-            try {
-                value = scanner.nextInt();
-                scanner.nextLine();
-                break;
-            } catch (InputMismatchException e) {
-                throw new RuntimeException("input time must be  INTEGER");
-            }
-
+        while (!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.out.print("Неверный ввод. ");
         }
+        value = scanner.nextInt();
+
         return value;
     }
 
@@ -26,18 +21,15 @@ public class TaskInput {
         @SuppressWarnings("resources")
         Scanner scanner = new Scanner(System.in);
         double value;
-        while (true) {
-            try {
-                value = scanner.nextDouble();
-                scanner.nextLine();
-                break;
-            } catch (InputMismatchException e) {
-                throw new RuntimeException("input time must be  Double");
-            }
-
+        while (!scanner.hasNextDouble()) {
+            scanner.nextLine();
+            System.out.print("Неверный ввод. ");
         }
+        value = scanner.nextDouble();
+
         return value;
     }
+
     public double[] readThreeNums(){
         double [] nums = new double[3];
         nums[0] = readDoubleFromConsole();
